5cdcdff2a4c13cbf83790963338ccc06a400ba22
Zachary Durr <zdurr22@gmail.com>
2020-10-27 23:19:21 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,7 @@ class String
   end
 
   def count_sentences
-    self.chomp.split.count
-    binding.pry
+    new_array = self.split(/[.?!]/).reject {|string| string.empty?}
+    return new_array.length
   end
 end
